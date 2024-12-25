4d2c29f1c07e4ca8ed960f926dceb582b3ce7ca1
bhumit <bhumitsachaniya1010@gmail.com>
2024-12-25 15:55:42 +0530

    project done

--- script.js
+++ script.js
@@ -5,15 +5,31 @@ inputField.addEventListener("input", () => {
     inputField.value = inputField.value.replace(/[^0-9+\-*/().]/g, "");
     console.log(inputField.value)
 });
-
+let str = "/01234567899+-*."
 document.addEventListener('keydown', function (e) {
-    if (e.key === 'Enter') {
-            const result = eval(inputField.value); 
-            inputField.value = result; 
-            console.log(result);
+    if (e.key === 'Enter') 
+    {
+        const result = eval(inputField.value); 
+        inputField.value = result; 
+        console.log(result);
+        console.log(e.key);
         e.preventDefault(); 
-    } else {
-        console.log(e);
+    } 
+    else if(e.key === 'Backspace')
+    {
+        inputField.value = inputField.value.slice(0, -1);
+        console.log(e.key);
+        e.preventDefault(); 
     }
-    console.log(inputField.value);
+    else if (str.includes(e.key)==true)
+    {
+        inputField.value += e.key
+        console.log(e.key);
+        e.preventDefault(); 
+    }
+    console.log(e.key);
 });
+
+let str2 = "hello world"
+let test = str2.includes('o')
+console.log (test)
