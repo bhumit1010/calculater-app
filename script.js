bf50d07fe4774c5584bf9505d070d93fc2db361e
Hetansh <hetanshsachaniya07@gmail.com>
2024-12-25 15:05:08 +0530

    Error Solved

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-let inputField= document.getElementById("display")
+let inputField = document.getElementById("display")
 
 inputField.addEventListener("input", () => {
     inputField.value = ''
     inputField.value = inputField.value.replace(/[^0-9+\-*/().]/g, "");
     console.log(inputField.value)
-  });
+});
 
-document.addEventListener('keydown', function(e){
+document.addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
-
-        console.log(inputField.value);
-        inputField.value= eval(inputField.value.toString())
-        inputField = inputField.value.slice(0, -1)
-        console.log(inputField.value);
-        
+            const result = eval(inputField.value); 
+            inputField.value = result; 
+            console.log(result);
+        e.preventDefault(); 
+    } else {
+        console.log(e);
     }
-    else{
-        console.log(e)
-    }
-
-})
+    console.log(inputField.value);
+});
